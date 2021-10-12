f7f75d69b831c9b412bc2145d5a55e8bd890d566
aaaaaa <tomato6726@gmail.com>
2021-10-12 16:42:42 +0900

    .

---
+++ test.js
@@ -0,0 +1,19 @@
+// let http = require('http')
+
+// const server = http.createServer(function(request, response){
+//     response.writeHead(200,{'Content-Type':'text/html'})
+//     response.end('Hello node.js!!')
+// })
+
+// server.listen(3000, function()
+// {
+//     console.log('Server is running...')
+// })
+
+console.log('Hello')
+
+setTimeout(() => {
+    console.log('Bye')
+}, 3000)
+
+console.log('Hello Again')
