8516b8bfe292fd3a2cdf15fd6579b51cc10dadfd
aaaaaa <tomato6726@gmail.com>
2021-11-03 13:59:47 +0900

    .

---
+++ zero/test2/main.js
@@ -0,0 +1,14 @@
+import express from 'express'
+import fs, { readFile } from 'fs'
+const app = express()
+
+app.get('/', (req, res) => {
+  readFile('./zero/test2/index.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(data)
+  })
+})
+
+app.listen(3000, () => {
+  console.log('server running..')
+})
