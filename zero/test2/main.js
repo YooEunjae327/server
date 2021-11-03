656b9bbfddf20c5ea9b780a7c788df37ba5c0ebf
aaaaaa <tomato6726@gmail.com>
2021-11-03 16:49:47 +0900

    .

--- zero/test2/main.js
+++ zero/test2/main.js
@@ -2,7 +2,18 @@ import express from 'express'
 import fs, { readFile } from 'fs'
 const app = express()
 
+app.use('/', (req, res) => {
+  readFile('./zero/test2/index.html', (err, data) => {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.end(data)
+  })
+})
+
 app.get('/', (req, res) => {
+  console.log('get')
+})
+
+app.post('/user', (req, res) => {
   readFile('./zero/test2/index.html', (err, data) => {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.end(data)
