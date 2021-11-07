7d44c8da4e04d913c8c3e47df546198a5b73f3b7
aaaaaa <tomato6726@gmail.com>
2021-11-07 20:54:18 +0900

    .

---
+++ myexam/main.js
@@ -0,0 +1,43 @@
+import express from 'express'
+import fs, { readFile } from 'fs'
+import path from 'path'
+import mysql from 'mysql'
+import bodyParser from 'body-parser'
+import { resourceUsage } from 'process'
+
+const conn = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: '12345678',
+  port: '3306',
+  database: 'teambuilding',
+})
+const app = express()
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.set('view engine', 'ejs')
+app.set('views', './myexam/view')
+// app.set('port', process.env.port || 3000)
+
+// main loginvews
+app.get('/loginviews', (req, res) => {
+  readFile('./myexam/view/index.html', (err, data) => {
+    res.send(data.toString())
+  })
+})
+
+// DB SELECT
+app.get('/readall', (req, res) => {
+  const sql = 'SELECT * FROM student'
+  conn.query(sql, (err, result) => {
+    if (err) {
+      console.log('err')
+      res.status(500).send('Internal Server Error')
+    }
+    res.render('readall', { student: result })
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server running...')
+})
