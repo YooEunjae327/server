53d855ca03a0d37341ae47a5b439205da7ad216d
aaaaaa <tomato6726@gmail.com>
2021-11-16 10:58:51 +0900

    Update router.js

--- myexam/router.js
+++ myexam/router.js
@@ -1,7 +1,10 @@
 import express from 'express'
-import insert from './router/insert.js'
+import signup from './router/signup.js'
+import login from './router/login.js'
+
 const app = express()
 
-app.use('/', insert)
+app.use('/signup', signup)
+app.use('/login', login)
 
 export default app
