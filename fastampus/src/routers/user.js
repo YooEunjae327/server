d39754538064ab3dbf5eb22ed5b1b6ec372b04f5
aaaaaa <tomato6726@gmail.com>
2021-11-18 21:18:47 +0900

    .

--- fastampus/src/routers/user.js
+++ fastampus/src/routers/user.js
@@ -1,12 +1,16 @@
 import express from 'express'
+import multer from 'multer'
 const router = express.Router()
+const upload = multer({ dest: 'uploads/' })
 
 const USERS = {
   15: {
     nickname: 'foo',
+    profileImage: undefined,
   },
   16: {
     nickname: 'bar',
+    profileImage: undefined,
   },
 }
 
@@ -17,7 +21,6 @@ router.get('/', (req, res) => {
 router.param('id', async (req, res, next, value) => {
   try {
     const user = USERS[value]
-
     if (!user) {
       const err = new Error('User not found')
       err.statusCode = 404
@@ -38,12 +41,22 @@ router.get('/:id', (req, res) => {
   } else if (resMimeType === 'html') {
     res.render('index', {
       nickname: req.user.nickname,
+      userId: req.params.id,
+      profileimgurl: 'fastampus/src/uploads/e6e2cbb267303c82a5fd04e2c8136825',
     })
   }
 })
 
-router.post('/', (req, res) => {
-  res.send('User registered')
+router.post('/:id/profile', upload.single('profile'), (req, res, next) => {
+  // const { user } = req
+  // user.profileImage
+  console.log(req.file)
+
+  // const { user } = req
+  // const { filename } = req.file
+  // user.profileImage = filename
+
+  res.send('User profile image uploaded')
 })
 
 export default router
