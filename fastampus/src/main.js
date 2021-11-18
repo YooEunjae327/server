71c3a980ee011e5b66e068f3631665f738f75816
aaaaaa <tomato6726@gmail.com>
2021-11-18 21:18:41 +0900

    .

--- fastampus/src/main.js
+++ fastampus/src/main.js
@@ -1,17 +1,8 @@
-import express from 'express'
-import router from './routers/user.js'
-const app = express()
+// https://jestjs.io/
+// https://www.npmjs.com/package/supertest
+// https://www.npmjs.com/package/multer
+import app from './app.js'
 
-app.use(express.json())
-app.use(express.static('fastampus/src/public'))
-app.set('views', './fastampus/src/views')
-app.set('view engine', 'pug')
-
-app.use('/users', router)
-app.use((err, req, res, next) => {
-  res.statusCode = err.statusCode || 500
-  res.send(err.message)
-})
 app.listen(3000, () => {
   console.log('Server running...')
 })
