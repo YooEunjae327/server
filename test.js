740330b01e843ec576383d1c982d3498a48762cf
aaaaaa <tomato6726@gmail.com>
2021-11-18 21:19:02 +0900

    .

---
+++ test.js
@@ -0,0 +1,17 @@
+const USERS = {
+  15: {
+    nickname: 'foo',
+    profileImage: undefined,
+  },
+  16: {
+    nickname: 'bar',
+    profileImage: undefined,
+  },
+  yoo: {
+    nickname: 'sex',
+    profileImage: undefined,
+  },
+}
+
+console.log(USERS[15].nickname)
+console.log(USERS.yoo.nickname)
